00059ece8574e0756727ba9512231096cb3abb8a
yuratkachukcs50 <tkachukyurii@gmail.com>
2016-10-20 14:56:21 +0300

    Create marioless.c

---
+++ marioless.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        printf("Height: ");
+        n = get_int();
+    }
+    while (n < 0 || n > 23);
+    for (int r = n; r > 0; r--)
+    {
+        int s = r - 1;
+        for ( int i = 0; i < s; i++)
+        {
+            printf(" ");
+        }
+        int hash = n - r + 1;
+        for (int i = 0; i < hash + 1; i++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
